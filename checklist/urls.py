1bbf9798d59e4d5d73283e6c4bbb1577a564e20d
Jerome Gu <xiaoxiaogu@gmail.com>
2016-05-17 23:42:57 +0800

    Day 2 on Learning django

--- checklist/urls.py
+++ checklist/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index')
+	url(r'^$', views.index, name='index'),
+	url(r'^(?P<year>[0-9]+)/(?P<month>[0-9]+)/(?P<day>[0-9]+)/$', views.checklist, name='checklist'),
 ]
 
--- checklist/views.py
+++ checklist/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def index(request):
 	return HttpResponse("Hello, world. You're at the checklist homepage.")
+
+def checklist(request, year, month, day):
+	return HttpResponse("You're checking the daily checklist.")
